6b7547c0c1681f71412b89b760035b5dfd1293be
Samreenfatma <samreenqutub@gmail.com>
2018-12-15 18:34:53 +0200

    old project has been removed

--- FizzBuzzSolution/src/main/java/com/modal/IresponseService.java
+++
@@ -1,8 +0,0 @@
-package com.modal;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface IresponseService {
-		public FizzBuzzResponse getResponse(FizzBuzzNumber numberList);
-}
